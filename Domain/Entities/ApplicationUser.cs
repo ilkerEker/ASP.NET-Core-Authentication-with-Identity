c3d3bcdbc887e9cf1ba60d5f8027e62d1b56b69d
ilker <lkrkr@hotmail.com>
2025-01-19 15:49:38 +0300

    Add project files.

---
+++ Domain/Entities/ApplicationUser.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace DotnetAuth.Domain.Entities
+{
+    public class ApplicationUser:IdentityUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Gender { get; set; }
+        public string? RefreshToken { get; set; }
+    }
+}
